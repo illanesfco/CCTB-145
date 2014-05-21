1cf1d5be1ebb3afcb9434ae50a082251b224326c
Francisco Illanes <illanesfco@gmail.com>
2014-05-21 15:15:08 -0600

    Assignment 2
    An exercise using boolean expressions

---
+++ Assignment2 Solution/Assignment2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool attendingSchool = true;
+            bool workingFullTime = false;
+            string name = "Francisco Illanes";
+            string city = "Calgary";
+            int yearsCollege = 2;
+            int yearsUniversity = 4;
+
+            int totalPostSecondaryYears = yearsCollege + yearsUniversity;
+            bool moreThan4YearsPostSecondary = totalPostSecondaryYears > 4;
+            bool attendingSchoolandWorking = attendingSchool && workingFullTime;
+            bool hardWorkingOrSmart = attendingSchool || workingFullTime;
+
+            Console.WriteLine(name + " from " + city);
+            Console.WriteLine("Attending School and Working: " + attendingSchoolandWorking);
+            Console.WriteLine("More than 4 years of Post Secondary: " + moreThan4YearsPostSecondary);
+            Console.WriteLine("Hard Working or Smart: " + hardWorkingOrSmart);
+
+        }
+    }
+}
