9daa26ce245f4563ba529f648b9acbfd9c70bbc3
Francisco Illanes <illanesfco@gmail.com>
2014-05-21 15:56:22 -0600

    Started Chapter 3 Samples
    If statements and basic method calling
    Completed up to p.26

---
+++ Chapter 3/ProgramFlow1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramFlow1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Write the code that will run the samples...
+            Program myApp = new Program(); // create a new object based on this class
+
+            myApp.IfStatementSample(); // call (i.e.: execute) the method
+
+        } // end of the Main() method
+
+        public void IfStatementSample()
+        {
+            Console.WriteLine("If Statement Sample...");
+            Console.Write("Enter your age: ");
+            string stringAge = Console.ReadLine();
+            int age = int.Parse(stringAge); //Convert the typed number to an int
+
+            if (age >= 18)
+                Console.WriteLine("You are an adult.");
+            else
+                Console.WriteLine("You are not an adult.");
+        } // end of IfStatementSample() method
+
+    } // end of the Program class
+
+} // end of the namespace for this file's code
