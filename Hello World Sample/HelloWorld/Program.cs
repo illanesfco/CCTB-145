8e764bc17de96974713863ac52fc819d3bed7f2d
Francisco Illanes <illanesfco@gmail.com>
2014-05-21 13:26:59 -0600

    Hello World Sample
    Intro sample

---
+++ Hello World Sample/HelloWorld/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Tell me your name: ");
+
+            // Create some local variables
+            string name; //declaration
+            name = Console.ReadLine(); //assignment
+            Console.WriteLine("Hello " + name);
+
+            int five = 5; //initialization (declaration + assignment)
+            int seven = 7;
+            int total = five + seven;
+            Console.WriteLine("Five + Seven is " + total);
+
+            double price = 10.95;
+            int quantity = 3;
+            double totalprice;
+            totalprice = price * quantity;
+            Console.Write(price);
+            Console.Write(" times ");
+            Console.Write(quantity + " equals ");
+            Console.WriteLine(totalprice);
+        }
+    }
+}
