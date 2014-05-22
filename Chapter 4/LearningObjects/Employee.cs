6cc68a7cedaa3ce0a37f2eb37d365b78bd9e2194
Francisco Illanes <illanesfco@gmail.com>
2014-05-22 15:49:01 -0600

    Added Chapter 4 Code Sample
    Covers creation and manipulation of objects

---
+++ Chapter 4/LearningObjects/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningObjects
+{
+    class Employee
+    {
+        // Declare variables for the class (auto-implemented properties)
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+        public DateTime StartingDate { get; set; }
+        public string PhoneNumber { get; set; }
+
+        // Declare constructors
+        // A) a paramerless (default) constructor
+        public Employee()
+        {
+        }
+
+        // B) a "greedy" constructor
+        public Employee(string name, int age, double salary, DateTime startedOn, string phone)
+        {
+            // TODO: Write code for the body of the greedy constructor
+            // Take all the values passed into the parameter variables, and store them in the properties
+            // The purpose of a constructor is to put meaningful information into properties and fields
+            Name = name;
+            Age = age;
+            Salary = salary;
+            StartingDate = startedOn;
+            PhoneNumber = phone;
+        }
+
+        // Method to assign an increase to the salary
+        public void AssignBonus(double increasePercentage)
+        {
+            // TODO: validate the increasePercentage first
+            Salary += Salary * increasePercentage;
+        }
+
+        //Override ToString()
+        public override string ToString()
+        {
+            return Name + " earns $" + Salary + " and started on " + StartingDate.ToShortDateString();
+        }
+    }
+}
---
+++ Chapter 4/LearningObjects/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myApp = new Program(); // calls the default constructor
+
+            Employee headProgrammer = myApp.CreateHeadProgrammer();
+            Employee bossesNephew = myApp.CreateStudentProgrammer(25000);
+            Employee saitStudent = myApp.CreateStudentProgrammer(21000);
+
+            myApp.CompareSalary(saitStudent, bossesNephew);
+
+            myApp.ApplyRaise(headProgrammer);
+            Console.WriteLine(headProgrammer.ToString());
+        }
+
+        private void ApplyRaise(Employee someone)
+        {
+            //Ask user for a raise amount, between 2 and 10 inclusive
+            Console.Write("Enter raise percentage (2-10%): ");
+            double raiseAmount = Double.Parse(Console.ReadLine());
+            while (raiseAmount < 2 || raiseAmount > 10)
+            {
+                Console.WriteLine("Not a valid number!");
+                Console.Write("Enter raise amount (2-10): ");
+                raiseAmount = Double.Parse(Console.ReadLine());
+            }
+            raiseAmount = raiseAmount / 100; //convert into a percentage
+            // Change the employee's salary
+            someone.AssignBonus(raiseAmount);
+            
+        }
+
+        private void CompareSalary(Employee personA, Employee personB)
+        {
+            double salaryDifference = personA.Salary - personB.Salary;
+            //get the absolute value
+            salaryDifference = Math.Abs(salaryDifference);
+            string paidTheMost;
+            if (personA.Salary > personB.Salary)
+                paidTheMost = personA.Name;
+            else
+                paidTheMost = personB.Name;
+
+            Console.WriteLine("{0} is paid ${1} more", paidTheMost, salaryDifference);
+        }
+
+        private Employee CreateStudentProgrammer(double startingWage)
+        {
+            Console.Write("Enter the student's name: ");
+            // Create the employee object by calling the greedy constructor
+            Employee cheapWorker = new Employee(Console.ReadLine(), 20, startingWage, DateTime.Today, "780.555.2003");
+            return cheapWorker;
+        }
+
+
+        private Employee CreateHeadProgrammer()
+        {
+            // Instatiate (create) an object using an Initializer List
+            Employee allan = new Employee()
+            {
+                Name = "Allan",
+                Age = 27,
+                Salary = 75000,
+                StartingDate = new DateTime(2010, 10, 28),
+                PhoneNumber = "780.555.1212"
+            };
+            //return the data
+            return allan;
+        }
+    }
+}
