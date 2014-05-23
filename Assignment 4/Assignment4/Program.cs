d9243e1d2c265dd13ce477f910c8c59936d0c83c
Francisco Illanes <illanesfco@gmail.com>
2014-05-22 23:28:54 -0600

    Assignment 4
    Student Class with basic property manipulation/output.

---
+++ Assignment 4/Assignment4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myApp = new Program();
+            Console.Write("Please enter the name of the new student: ");
+            Student fran = new Student(0001, Console.ReadLine(), "1234");
+            fran.OutputStudentDetails();
+            Console.WriteLine("Adding a course to student's history");
+            fran.IncreaseCoursesTaken();
+            fran.OutputStudentDetails();
+
+            
+        }
+    }
+}
---
+++ Assignment 4/Assignment4/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class Student
+    {
+        //Properties
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private string StudentId { get; set; }
+        private int CoursesTaken { get; set; }
+
+        //Constructos
+        public Student()
+        {
+
+        }
+
+        public Student(int id, string name, string studentId)
+        {
+            Id = id;
+            Name = name;
+            StudentId = studentId;
+            CoursesTaken = 0;
+        }
+
+        //Methods
+        public void IncreaseCoursesTaken()
+        {
+            CoursesTaken++;
+        }
+
+        public void OutputStudentDetails()
+        {
+            Console.WriteLine("{0} (Student ID: {1}) has taken {2} course(s)", Name, StudentId, CoursesTaken);
+        }
+
+        
+
+
+
+
+    }
+}
